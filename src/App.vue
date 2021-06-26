c749589f025545cd46e9dbbeaf48b3a4e9c5cf1d
NicoPache <nepacheco94@gmail.com>
2021-06-26 16:44:55 -0300

    first Commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <br>
+   <Juego/>
+  </div>
+</template>
+
+<script>
+import Juego from './components/juego.vue'
+export default {
+  name: 'App',
+  components: {
+   Juego,
+  }
+}
+</script>
+
+<style>
+body{
+  background-color: #232323;
+  margin-top: -20px;
+  margin-left: -8px;
+  margin-right: -8px;
+  font-family: "Montserrat", "Avenir";
+}
+h1{	font-weight: normal;
+	line-height: 1.1;
+	padding: 20px 0;
+} 
+</style>
